856318e7f5516754dddacf712efcfdfb1d598a92
Zwiikty <zwiikty@gmail.com>
2025-06-11 17:14:02 +0700

    add cors

--- src/app.js
+++ src/app.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 
-app.use(express.json());
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/bookings', require('./routes/booking.routes'));
 app.use('/api/courts', require('./routes/court.routes'));
 app.use('/api/payment', require('./routes/payment.routes'));
 app.use('/slips', express.static(path.join(__dirname, 'src/uploads/slips')));
 
+app.use(express.json());
 module.exports = app;
 
